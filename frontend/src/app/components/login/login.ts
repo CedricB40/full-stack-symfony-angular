ec2f1a5cbf01a2fa4e47d4e283b7728da317ff68
CedricB40 <bazinc40@gmail.com>
2026-09-05 21:27:23 +0200

    connexion auth.ts et login.ts ok + json-server

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,21 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
   imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
-  email = signal("")
-  password = signal("")
+  service = inject(Auth);
+  email = '';
+  password = '';
 
-  login(){
-    console.log(this.email());
-    console.log(this.password());
-    
-    
+  login() {
+    this.service.login(this.email).subscribe({
+      next: (data: any[]) => {
+        console.log('DATA =', data);
+        if (data.length > 0 && data[0].password === this.password) {
+          console.log('Connexion réussie');
+          console.log('Utilisateur ', data[0]);
+        } else {
+          console.log('Email ou mot de passe incorrect');
+        }
+      },
+      error: (erreur: any) => {
+        console.log('Erreur de connexion', erreur);
+      }
+    });
   }
-
 }
--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+  private http = inject(HttpClient);
+  private api = 'http://localhost:3000/users';
+
+  login(email: string) {
+    return this.http.get<any[]>(`${this.api}?email=${encodeURIComponent(email)}`);
+  }
+}
